c294f2d03d966cfa573504fb8e41639b8fbe09a2
fiftie <kumavichi.t@gmail.com>
2019-04-20 18:05:34 +0900

    streamingテスト

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname,'stream')));
+app.get('/' , function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(PORT, function(){
+  console.log('server listening. Port:' + PORT);
+});
